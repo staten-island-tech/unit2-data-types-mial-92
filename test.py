2b940770c1a025d1512006bb7f9a967c93946914
mial-92 <mialin0170@gmail.com>
2025-09-12 07:06:34 -0700

    Update test.py

--- test.py
+++ test.py
@@ -1,8 +1,10 @@
 """ x = 3
 y = float(3)
 print(x,y) """
-values = [1,2.23,5,7,2,30,15]
-for i in values:
-    print(values[0])
-    print(values[6])
-""" answer1 = input("") """
+""" values = [1,2.23,5,7,2,30,15]
+print(values[0])
+print(values[5])
+ answer1 = input("")  """
+
+answer1 = input("What's your name?")
+answer2 = input(f"Hello {answer1}")
