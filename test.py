024a661a4dd5a5f47562beb36d31edd0ac658c2e
mial-92 <mialin0170@gmail.com>
2025-09-16 07:16:34 -0700

    Update test.py

--- test.py
+++ test.py
@@ -5,7 +5,7 @@ print(x,y) """
 print(values[0])
 print(values[5])
  answer1 = input("")  """
-
+""" 
 answer1 = input("What's your name?")
 print(f"Hello {answer1}")
 answer3 = input("What day is it for you?")
@@ -18,4 +18,12 @@ answer6 = input("Who's your best friend/favorite person?")
 print("cool")
 answer7 = input("When do you leave school?")
 print("yippee!")
-print(f"{answer1} was on their way to school on a normal {answer3} when suddenly a biker on a bike passes by at what felt like 200 miles per hour. You spin and nearly fall, balancing on your feet and all of a sudden a flock of seagulls fly over you, and drops of bird dookie start raining over you. You skillfully dodge the dookie-rain because you're just that cool. Later in the day, you go to your favorite class, {answer4} and have a blast. You leave school at {answer7} and see an ice cream truck. You think to yourself 'hmmmm i really want some ice cream right now.' You make your way in line and when it's your turn, you pay for your favorite ice cream {answer5}. Then all of a sudden, a wild and coocoo {answer6} comes sneak attacking you, making you drop your delicious {answer6} flavored ice cream. Now you have no ice cream. :C)")
+print(f"{answer1} was on their way to school on a normal {answer3} when suddenly a biker on a bike passes by at what felt like 200 miles per hour. You spin and nearly fall, balancing on your feet and all of a sudden a flock of seagulls fly over you, and drops of bird dookie start raining over you. You skillfully dodge the dookie-rain because you're just that cool. Later in the day, you go to your favorite class, {answer4} and have a blast. You leave school at {answer7} and see an ice cream truck. You think to yourself 'hmmmm i really want some ice cream right now.' You make your way in line and when it's your turn, you pay for your favorite ice cream {answer5}. Then all of a sudden, a wild and coocoo {answer6} comes sneak attacking you, making you drop your delicious {answer5} flavored ice cream. Now you have no ice cream. :C)") """
+
+def odd_or_even(x):
+    if x % 2 == 0:
+        print("even")
+    else: 
+       print("odd")
+odd_or_even(19)
+
